da292ad3cdc1a8fe7ca9a74febe561f9962fa591
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2024-05-14 14:04:12 -0700

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+rem Downloading check_minecraft_conflicts.py from GitHub
+echo Downloading check_minecraft_conflicts.py...
+curl -o check_minecraft_conflicts.py https://raw.githubusercontent.com/DevilGlitch/Minecraft-Mod-Compatibility-Checker/main/check_minecraft_conflicts.py
+
+rem Running check_minecraft_conflicts.py using the current Python installation
+echo Running check_minecraft_conflicts.py...
+python check_minecraft_conflicts.py
+
+rem Open conflicting_jars.txt
+echo Opening conflicting_jars.txt...
+start conflicting_jars.txt
+
+echo Finished.
+pause
