f1a182d3c66d6fdcb99812d41279ce7a412525b8
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2024-05-15 23:13:17 -0700

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -3,7 +3,7 @@ setlocal
 
 rem Downloading check_minecraft_conflicts.py from GitHub
 echo Downloading check_minecraft_conflicts.py...
-curl -o check_minecraft_conflicts.py https://raw.githubusercontent.com/DevilGlitch/Minecraft-Mod-Compatibility-Checker/main/check_minecraft_conflicts.py
+curl -o check_minecraft_conflicts.py https://raw.githubusercontent.com/DevilGlitch/ezMMCC/main/check_minecraft_conflicts.py
 
 rem Running check_minecraft_conflicts.py using the current Python installation
 echo Running check_minecraft_conflicts.py...
